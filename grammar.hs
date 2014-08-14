7c5f285d309b6898d11f73102e728cc4e8f5ed68
Nate Bragg <Nate_Bragg@bose.com>
2014-08-14 07:45:38 -0400

    Added state machine and state list parsing.
    --HG--
    branch : haskell-example

--- grammar.hs
+++ grammar.hs
@@ -3,6 +3,18 @@ module Grammar where
 import Text.ParserCombinators.Parsec
 import Control.Applicative hiding ((<|>), empty)
 
+state_machine :: Parser (String, [(String, [(String, Maybe String)])])
+state_machine =
+    do sm <- state_machine_name
+       empty
+       char '{'
+       empty
+       sl <- state_list
+       empty
+       char '}'
+       empty
+       return (sm, sl)
+
 sep :: Parser Char
 sep = oneOf "-_"
 
@@ -20,8 +32,26 @@ identifier = many1 (alphaNum <|> (char '-'))
              <|> (:) <$> id_char <*> (many1 id_char)
              <|> ((char '"') >> many1 (id_char <|> space <|> symbol) <* (char '"'))
 
+state_machine_name :: Parser String
+state_machine_name = identifier
+
+state :: Parser (String, [(String, Maybe String)])
+state = 
+    do sn <- state_name 
+       empty
+       char '['
+       empty
+       el <- event_handler_list
+       empty
+       char ']'
+       empty
+       return (sn, el)
+
+state_list :: Parser [(String, [(String, Maybe String)])]
+state_list = sepBy (state <* empty) (char ',' >> empty)
+
 event_handler_list :: Parser [(String, Maybe String)]
-event_handler_list = sepBy (empty *> event_handler) (empty >> char ',')
+event_handler_list = sepBy (event_handler <* empty) (char ',' >> empty)
 
 event_handler :: Parser (String, Maybe String)
 event_handler =

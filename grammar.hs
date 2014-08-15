480379d6ab699c391ffe032d72be8c99f0371528
Nate Bragg <Nate_Bragg@bose.com>
2014-08-15 14:37:49 -0400

    Promoted states and events into proper types, added "any" token
    --HG--
    branch : haskell-example

--- grammar.hs
+++ grammar.hs
@@ -1,27 +1,33 @@
 module Grammar where
 
-import Text.ParserCombinators.Parsec
+import Text.ParserCombinators.Parsec hiding (State)
 import Control.Applicative hiding ((<|>), empty, many)
 
-state_machine :: Parser (String, [(String, Maybe String, [(String, (Maybe [(Maybe String, Maybe (Maybe String, String))], Maybe String))], Maybe String)])
+data State = State String | StateAny
+    deriving (Show, Eq, Ord)
+
+data Event = Event String | EventAny | EventEnter
+    deriving (Show, Eq, Ord)
+
+state_machine :: Parser (String, [(State, Maybe String, [(Event, (Maybe [(Maybe String, Maybe (Maybe String, Event))], Maybe State))], Maybe String)])
 state_machine = (,) <$> (empty *> state_machine_name <* empty) <*> state_machine_spec <* empty
 
-state_machine_spec :: Parser [(String, Maybe String, [(String, (Maybe [(Maybe String, Maybe (Maybe String, String))], Maybe String))], Maybe String)]
+state_machine_spec :: Parser [(State, Maybe String, [(Event, (Maybe [(Maybe String, Maybe (Maybe String, Event))], Maybe State))], Maybe String)]
 state_machine_spec = (char '{' >> empty) *> state_list <* (empty >> char '}')
 
-state_list :: Parser [(String, Maybe String, [(String, (Maybe [(Maybe String, Maybe (Maybe String, String))], Maybe String))], Maybe String)]
+state_list :: Parser [(State, Maybe String, [(Event, (Maybe [(Maybe String, Maybe (Maybe String, Event))], Maybe State))], Maybe String)]
 state_list = sepBy (state <* empty) (char ',' >> empty)
 
-state :: Parser (String, Maybe String, [(String, (Maybe [(Maybe String, Maybe (Maybe String, String))], Maybe String))], Maybe String)
+state :: Parser (State, Maybe String, [(Event, (Maybe [(Maybe String, Maybe (Maybe String, Event))], Maybe State))], Maybe String)
 state = try ((,,,) <$> state_title <* empty <*> return Nothing
-                   <*> (to_state >>= \(ses, st) -> return [("", (ses, Just st))]) <*> return Nothing)
+                   <*> (to_state >>= \(ses, st) -> return [(EventEnter, (ses, Just st))]) <*> return Nothing)
          <|> (,,,) <$> state_title <* empty <*> enter_function <* empty
                    <*> event_handler_spec <* empty <*> exit_function
 
-event_handler_spec :: Parser [(String, (Maybe [(Maybe String, Maybe (Maybe String, String))], Maybe String))]
+event_handler_spec :: Parser [(Event, (Maybe [(Maybe String, Maybe (Maybe String, Event))], Maybe State))]
 event_handler_spec = (char '[' >> empty) *> event_handler_list <* (empty >> char ']')
 
-event_handler_list :: Parser [(String, (Maybe [(Maybe String, Maybe (Maybe String, String))], Maybe String))]
+event_handler_list :: Parser [(Event, (Maybe [(Maybe String, Maybe (Maybe String, Event))], Maybe State))]
 event_handler_list = sepBy (event_handler <* empty) (char ',' >> empty)
 
 enter_function :: Parser (Maybe String)
@@ -30,61 +36,62 @@ enter_function = optionMaybe function_call
 exit_function :: Parser (Maybe String)
 exit_function = optionMaybe function_call
 
-side_effect_container :: Parser [(Maybe String, Maybe (Maybe String, String))]
+side_effect_container :: Parser [(Maybe String, Maybe (Maybe String, Event))]
 side_effect_container = (char '(' >> empty) *> side_effect_list <* (empty >> char ')')
 
-to_state :: Parser (Maybe [(Maybe String, Maybe (Maybe String, String))], String)
+to_state :: Parser (Maybe [(Maybe String, Maybe (Maybe String, Event))], State)
 to_state = (,) <$> arrow <* empty <*> state_name
 
-dash :: Parser (Maybe [(Maybe String, Maybe (Maybe String, String))])
+dash :: Parser (Maybe [(Maybe String, Maybe (Maybe String, Event))])
 dash = (char '-') *> optionMaybe side_effect_container <* (char '-')
 
-arrow :: Parser (Maybe [(Maybe String, Maybe (Maybe String, String))])
+arrow :: Parser (Maybe [(Maybe String, Maybe (Maybe String, Event))])
 arrow = dash <* (char '>')
 
---TODO
-event_handler :: Parser (String, (Maybe [(Maybe String, Maybe (Maybe String, String))], Maybe String))
+event_handler :: Parser (Event, (Maybe [(Maybe String, Maybe (Maybe String, Event))], Maybe State))
 event_handler =
-    do ev <- event_name
+    do ev <- event_name <|> event_any
        empty
        try (to_state >>= \(ses, st) -> return (ev, (ses, Just st)))
         <|> (dash <* empty >>= \ses -> return (ev, (ses, Nothing)))
-        <?> "state transition for event \"" ++ ev ++ "\""
+        <?> "state transition for event \"" ++ show ev ++ "\""
 
-side_effect_list :: Parser [(Maybe String, Maybe (Maybe String, String))]
+side_effect_list :: Parser [(Maybe String, Maybe (Maybe String, Event))]
 side_effect_list = sepBy (side_effect <* empty) (char ',' >> empty)
 
-side_effect :: Parser (Maybe String, Maybe (Maybe String, String))
+side_effect :: Parser (Maybe String, Maybe (Maybe String, Event))
 side_effect = try (typed_function_call >>= \f -> return (Just (fst f), Just (snd f)))
               <|> try ((,) <$> (function_call >>= \f -> return (Just f)) <*> return Nothing)
               <|> (,) <$> return Nothing <*> (qualified_event >>= \e -> return (Just e))
               <?> "side effect"
 
-typed_function_call :: Parser (String, (Maybe String, String))
+typed_function_call :: Parser (String, (Maybe String, Event))
 typed_function_call = (,) <$> function_call <* (empty >> char ':' >> empty) <*> qualified_event
 
-qualified_event :: Parser (Maybe String, String)
+qualified_event :: Parser (Maybe String, Event)
 qualified_event = try ((,) <$> optionMaybe (state_machine_name <* (char '.')) <*> event_name)
                   <|> (,) <$> return Nothing <*> event_name
 
 function_call :: Parser String
 function_call = char '@' *> c_identifier
 
---TODO
-state_title :: Parser String
-state_title = state_name
+state_title :: Parser State
+state_title = state_name <|> state_any
 
 state_machine_name :: Parser String
 state_machine_name = identifier
 
-state_name :: Parser String
-state_name = identifier
+state_name :: Parser State
+state_name = State <$> identifier
 
-event_name :: Parser String
-event_name = identifier
+event_name :: Parser Event
+event_name = Event <$> identifier
 
---TODO
---any
+state_any :: Parser State
+state_any = char '_' *> return StateAny
+
+event_any :: Parser Event
+event_any = char '_' *> return EventAny
 
 --TODO
 --comment
--- main.hs
+++ main.hs
@@ -1,10 +1,10 @@
 module Main where
 
-import Text.ParserCombinators.Parsec
+import Text.ParserCombinators.Parsec (parse, ParseError)
 import System.Environment
 import Grammar
 
-result :: Either ParseError (String, [(String, Maybe String, [(String, (Maybe [(Maybe String, Maybe (Maybe String, String))], Maybe String))], Maybe String)]) -> IO()
+result :: Either ParseError (String, [(State, Maybe String, [(Event, (Maybe [(Maybe String, Maybe (Maybe String, Event))], Maybe State))], Maybe String)]) -> IO()
 result (Left err) = print err
 result (Right s)  = print s
 

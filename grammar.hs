bb11827436f0dbbb5f29ff47df8ab322cbd172e8
Nate Bragg <Nate_Bragg@bose.com>
2014-08-15 07:55:51 -0400

    Added support for @function calls.
    --HG--
    branch : haskell-example

--- grammar.hs
+++ grammar.hs
@@ -1,7 +1,7 @@
 module Grammar where
 
 import Text.ParserCombinators.Parsec
-import Control.Applicative hiding ((<|>), empty)
+import Control.Applicative hiding ((<|>), empty, many)
 
 state_machine :: Parser (String, [(String, [(String, Maybe String)])])
 state_machine =
@@ -31,6 +31,12 @@ event_handler_spec = (char '[' >> empty) *> event_handler_list <* (empty >> char
 event_handler_list :: Parser [(String, Maybe String)]
 event_handler_list = sepBy (event_handler <* empty) (char ',' >> empty)
 
+enter_function :: Parser (Maybe String)
+enter_function = optionMaybe function_call
+
+exit_function :: Parser (Maybe String)
+exit_function = optionMaybe function_call
+
 state_list :: Parser [(String, [(String, Maybe String)])]
 state_list = sepBy (state <* empty) (char ',' >> empty)
 
@@ -41,6 +47,16 @@ event_handler =
        try (string "-->" >> empty >> state_name >>= \st -> return (ev, Just st))
         <|> (string "--" >> empty >> return (ev, Nothing))
         <?> "state transition for event \"" ++ ev ++ "\""
+
+typed_function_call :: Parser (String, (Maybe String, String))
+typed_function_call = (,) <$> function_call <* (empty >> char ':' >> empty) <*> qualified_event
+
+qualified_event :: Parser (Maybe String, String)
+qualified_event = try ((,) <$> optionMaybe (state_machine_name <* (char '.')) <*> event_name)
+                  <|> (,) <$> return Nothing <*> event_name
+
+function_call :: Parser String
+function_call = char '@' *> c_identifier
 
 state_machine_name :: Parser String
 state_machine_name = identifier
@@ -56,6 +72,9 @@ identifier = try ((:) <$> id_char <*> (many1 id_char))
              <|> many1 (alphaNum <|> (char '-'))
              <|> quoted
 
+c_identifier :: Parser String
+c_identifier = (:) <$> nondigit <*> many (nondigit <|> digit)
+
 sep :: Parser Char
 sep = oneOf "-_"
 
@@ -67,6 +86,9 @@ id_char = (alphaNum <|> sep)
 
 empty :: Parser ()
 empty = spaces
+
+nondigit :: Parser Char
+nondigit = letter <|> char '_'
 
 quoted :: Parser String
 quoted = ((char '"') *> many1 (id_char <|> space <|> symbol) <* (char '"'))

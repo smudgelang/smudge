90bd826ab6281350353f6c053a78993d75e65ee7
Nate Bragg <Nate_Bragg@bose.com>
2014-08-19 15:20:21 -0400

    Added side effect labels.

--- Main.hs
+++ Main.hs
@@ -7,7 +7,16 @@ import Grammar
 import Data.Graph.Inductive.Graph
 import Data.Graph.Inductive.PatriciaTree
 import Data.GraphViz hiding (parse)
+import Data.GraphViz.Attributes.Complete (Label(..))
+import Data.Text.Lazy.Internal (Text(..))
+import Data.Monoid
+import Data.List (intersperse)
 import qualified Data.Map as Map
+
+-- Sinful.  This instance is incomplete.
+instance Monoid Label where
+    mempty = StrLabel Empty
+    mappend (StrLabel a) (StrLabel b) = StrLabel (mappend a b)
 
 instance Labellable State where
     toLabelValue (State s) = toLabelValue s
@@ -17,11 +26,16 @@ instance Labellable Event where
     toLabelValue (Event e) = toLabelValue e
     toLabelValue e         = toLabelValue $ show e
 
+instance Labellable SideEffect where
+    toLabelValue (FuncVoid f) = toLabelValue f
+    toLabelValue (FuncEvent f (s, e)) = toLabelValue e
+    toLabelValue (FuncDefault (s, e)) = toLabelValue e
+
 data EventAndSideEffects = EventAndSideEffects Event [SideEffect]
     deriving (Show, Eq, Ord)
 
 instance Labellable EventAndSideEffects where
-    toLabelValue (EventAndSideEffects e ses) = toLabelValue e
+    toLabelValue (EventAndSideEffects e ses) = mconcat $ intersperse (toLabelValue "\n") $ toLabelValue e : map toLabelValue ses
 
 smToGraph :: (StateMachine, [(State, [(Event, [SideEffect], State)])]) -> Gr State EventAndSideEffects
 smToGraph (sm, ss) = mkGraph [s | s <- zip [1..] (map fst ss)] es

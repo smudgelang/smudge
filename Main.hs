7b6919c641068390bb09c482df8418093e9e0783
Nate Bragg <Nate_Bragg@bose.com>
2017-04-28 09:07:06 -0400

    Fix #53: File-level compilation switches
    Break apart system options to prevent "help" and "version" as pragmas.
    
    Revert special flower workaround for "--strict" counterexample.

--- Main.hs
+++ Main.hs
@@ -16,7 +16,7 @@ import Semantics.Solver (
   elaborateMono,
   elaboratePoly,
   )
-import Parsers.Smudge (state_machine, smudgle)
+import Parsers.Smudge (smudge_file)
 import Semantics.Alias (merge)
 import Semantics.Basis (basisAlias, bindBasis)
 import Semantics.Semantic (Severity(..), Fault(..), fatal)
@@ -28,7 +28,7 @@ import Control.Arrow (second)
 import Control.Applicative ((<$>), (<*>))
 import Distribution.Package (packageVersion, packageName, PackageName(..))
 import Text.Parsec (parse, eof)
-import System.Console.GetOpt (usageInfo, getOpt, OptDescr(..), ArgDescr(..), ArgOrder(..))
+import System.Console.GetOpt (usageInfo, getOpt, OptDescr(..), ArgDescr(..), ArgOrder(Permute))
 import System.Environment (getArgs)
 import System.FilePath (joinPath, takeFileName, dropFileName, normalise)
 import System.Exit (exitFailure)
@@ -50,10 +50,12 @@ subcommand name f os = map makeSub os
 
 data SystemOption = Version
                   | Help
-                  | Strict
+                  | OutDir FilePath
+    deriving (Show, Eq)
+
+data EnvmntOption = Strict
                   | Namespace String
                   | Rename String
-                  | OutDir FilePath
     deriving (Show, Eq)
 
 app_name :: String
@@ -67,16 +69,21 @@ header = "Usage: " ++ app_name ++ " [OPTIONS] file\n" ++
 sysopts :: [OptDescr SystemOption]
 sysopts = [Option ['v'] ["version"] (NoArg Version) "Version information.",
            Option ['h'] ["help"] (NoArg Help) "Print this message.",
-           Option []    ["strict"] (NoArg Strict) "Require all types to match strictly",
-           Option []    ["namespace"] (ReqArg Namespace "NEW") "Replace namespace.",
-           Option []    ["rename"] (ReqArg Rename "\"OLD NEW\"") "Replace identifier.",
            Option []    ["outdir"] (ReqArg OutDir "DIR") "Output directory."]
 
-data Options = SystemOption SystemOption | GraphVizOption GraphVizOption | CStaticOption CStaticOption
+envopts :: [OptDescr EnvmntOption]
+envopts = [Option []    ["strict"] (NoArg Strict) "Require all types to match strictly",
+           Option []    ["namespace"] (ReqArg Namespace "NEW") "Replace namespace.",
+           Option []    ["rename"] (ReqArg Rename "\"OLD NEW\"") "Replace identifier."]
+
+data Options = SystemOption SystemOption | EnvmntOption EnvmntOption | GraphVizOption GraphVizOption | CStaticOption CStaticOption
     deriving (Show, Eq)
 
 all_opts :: [OptDescr Options]
-all_opts = concat [subcommand "" SystemOption sysopts,
+all_opts = concat [subcommand "" SystemOption sysopts] ++ fileopts
+
+fileopts :: [OptDescr Options]
+fileopts = concat [subcommand "" EnvmntOption envopts,
                    (subcommand <$> fst <*> return CStaticOption <*> snd) options,
                    (subcommand <$> fst <*> return GraphVizOption <*> snd) options]
 
@@ -92,51 +99,63 @@ rename s = case map (second reads) $ reads s of
             [(a, [(b, "")])] -> Right (a, b)
             otherwise      -> Left s
 
---processFile :: String -> [Options] -> IO ([(StateMachine, Gr EnterExitState Happening)], Alias, SymbolTable)
+--processFile :: String -> [Options] -> IO ([(StateMachine TaggedName, Gr EnterExitState Happening)], Alias QualifiedName, SymbolTable)
 processFile fileName os = do
-    mapM (putStrLn . ("Parse error in rename flag: " ++)) (lefts renames)
-    when (any isLeft renames) exitFailure
     compilationUnit <-
         if fileName == "-"
             then getContents
             else readFile fileName
-    case parse (smudgle <* eof) fileName compilationUnit of
-        Left err -> print_exit (show err)
-        Right sms -> m sms
+    case parse (smudge_file <* eof) fileName compilationUnit of
+        Left err -> print err >> report_failure 1
+        Right (ps, sms) ->
+            let flatten (a, b, c) = (concat a ++ os, concat b, concat c)
+            in  case flatten $ unzip3 $ map (getOpt Permute fileopts) ps of
+                    (os, [], []) -> checkAndConvert sms os >>= make_output outputTarget os
+                    (_, fs, es) -> do putStr (concat es)
+                                      mapM (putStrLn . ("unrecognized argument `" ++) . (++ "'")) fs
+                                      report_failure (length fs + length es)
+    where outputTarget = normalise $ joinPath [(prefix os), takeFileName fileName]
+          prefix [] = dropFileName fileName
+          prefix ((SystemOption (OutDir p)):_) = p
+          prefix (_:t) = prefix t
+
+--checkAndConvert :: [(StateMachine Identifier, WholeState Identifier)] -> [Options] -> IO ([(StateMachine TaggedName, Gr EnterExitState Happening)], Alias QualifiedName, SymbolTable)
+checkAndConvert sms os = do
+    let rename_errors = lefts renames
+    mapM (putStrLn . ("Parse error in rename flag: " ++)) rename_errors
+    when (not $ null rename_errors) $ report_failure $ length rename_errors
+    m sms
     where
-        renames = map rename [r | SystemOption (Rename r) <- os]
-        namespace = last $ "SMUDGE" : [n | SystemOption (Namespace n) <- os]
+        renames = map rename [r | EnvmntOption (Rename r) <- os]
+        namespace = last $ "SMUDGE" : [n | EnvmntOption (Namespace n) <- os]
         aliases = merge (basisAlias namespace) $ fromList $ rights renames
         m sms = do
             let sms' = passInitialState sms
             let sms'' = passRename aliases $ passFullyQualify sms'
             let sms''' = passTagCategories sms''
             let fs = concat $ map name_passes sms'''
-            mapM (putStrLn . show) fs
+            mapM print fs
             when (any fatal fs) $ report_failure $ length fs
 
             let basis = bindBasis aliases $ map fst sms''
-            let st = if elem (SystemOption Strict) os
+            let st = if elem (EnvmntOption Strict) os
                      then elaborateMono basis sms'''
                      else elaboratePoly basis sms'''
             -- This is a bit of a hack around the definition of Passable
             let types = take 1 (zip (map fst sms''') (repeat st))
             let fs = concat $ map type_passes types
-            mapM (putStrLn . show) fs
+            mapM print fs
             when (any fatal fs) $ report_failure $ length fs
 
             let gs = passWholeStateToGraph sms'''
             let fs = concat $ map make_passes gs
-            mapM (putStrLn . show) fs
+            mapM print fs
             when (any fatal fs) $ report_failure $ length fs
 
             return (gs, aliases, st)
-        report_failure n =
-            print_exit ("Exiting with " ++ show n ++ " error" ++ (if n == 1 then "" else "s"))
-        print_exit e = do
-            putStrLn e
-            exitFailure
-            return mempty
+
+report_failure :: Int -> IO a
+report_failure n = putStrLn ("Exiting with " ++ show n ++ " error" ++ (if n == 1 then "" else "s")) >> exitFailure
 
 --make_output :: String -> [Options] -> ([(StateMachine, Gr EnterExitState Happening)], Alias, SymbolTable) -> IO ()
 make_output fileName os gswst = do
@@ -156,11 +175,6 @@ main = do
             | elem (SystemOption Help) os -> printUsage
             | elem (SystemOption Version) os -> printVersion
 
-        (os, (fileName:as),  _) ->
-            let outputTarget = normalise $ joinPath [(prefix os), takeFileName fileName]
-                prefix [] = dropFileName fileName
-                prefix ((SystemOption (OutDir p)):_) = p
-                prefix (_:t) = prefix t
-            in processFile fileName os >>= make_output outputTarget os
+        (os, (fileName:as),  _) -> processFile fileName os
 
         (_,              _, es) -> putStr (concat es) >> printUsage

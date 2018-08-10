14d37f5011e083c4c6670358778ab2fcc17967f4
Brian Dodge <brian.dodge@cambridgeconsultants.com>
2018-08-10 14:28:41 -0400

    PR feedback

--- src/Language/Smudge/Backends/SmudgeIR.hs
+++ src/Language/Smudge/Backends/SmudgeIR.hs
@@ -177,28 +177,28 @@ data Stmt x = Cases (Expr x) [(x, [Stmt x])] [Stmt x]
             | If (Expr x) [Stmt x]
             | Return (Expr x)
             | ExprS (Expr x)
-            | Voided (Expr x)
+            | Unused (Expr x)
 
 instance Functor Stmt where
     fmap f (Cases e cs ds) = Cases (fmap f e) (map (f *** map (fmap f)) cs) (map (fmap f) ds)
     fmap f (If e ss) = If (fmap f e) (map (fmap f) ss)
     fmap f (Return e) = Return $ fmap f e
     fmap f (ExprS e) = ExprS $ fmap f e
-    fmap f (Voided e) = Voided $ fmap f e
+    fmap f (Unused e) = Unused $ fmap f e
 
 instance Foldable Stmt where
     foldMap f (Cases e cs ds) = foldMap f e `mappend` foldMap (uncurry mappend . (f *** foldMap (foldMap f))) cs `mappend` foldMap (foldMap f) ds
     foldMap f (If e ss) = foldMap f e `mappend` foldMap (foldMap f) ss
     foldMap f (Return e) = foldMap f e
     foldMap f (ExprS e) = foldMap f e
-    foldMap f (Voided e) = foldMap f e
+    foldMap f (Unused e) = foldMap f e
 
 instance Traversable Stmt where
     traverse f (Cases e cs ds) = Cases <$> traverse f e <*> traverse (seqtup . (f *** traverse (traverse f))) cs <*> traverse (traverse f) ds
     traverse f (If e ss) = If <$> traverse f e <*> traverse (traverse f) ss
     traverse f (Return e) = Return <$> traverse f e
     traverse f (ExprS e) = ExprS <$> traverse f e
-    traverse f (Voided e) = Voided <$> traverse f e
+    traverse f (Unused e) = Unused <$> traverse f e
 
 data Expr x = FunCall x [Expr x]
             | Literal String
@@ -375,7 +375,7 @@ lowerMachine cfg ssyms (StateMachine smName, g') = [
                   name_var = qualify "event_name"
                   event_var = head eventNames
                   ds = if (not $ null handler) || not (debug cfg) then [] else [VarDef Unresolved $ SumVDec wrap_name (Ty eventEnum) $ Init (evt_id evName, Value $ Var event_var)]
-                  es =  (if (null ds) then [Voided (Value $ Var event_var)] else []) ++ (case handler of
+                  es =  (if (null ds) then [Unused (Value $ Var event_var)] else []) ++ (case handler of
                            [(s, e')] -> [ExprS $ FunCall (qualify (sName smName s, mangleEv e')) (if e == e' then [Value $ Var event_var] else [])]
                            [] -> call_panic_f [Literal panic_s, FunCall stateName_f [Value $ Var stateVar], FunCall eventName_f [Value $ Var wrap_name]])
                   panic_s = disqualifyTag smName ++ "{%s[%s]}: Unhandled event\n"
@@ -403,8 +403,7 @@ lowerMachine cfg ssyms (StateMachine smName, g') = [
         handleEventFun :: Event TaggedName -> [(State TaggedName, (State TaggedName, Event TaggedName))] -> [State TaggedName] -> Def QualifiedName
         handleEventFun e@(Event evName) ss unss = FunDef f_name eventNames (Internal, snd $ syms ! TagFunction (qualify evName)) [] es
             where f_name = qualify (qualify evName, "handle")
-                  es = vs ++ [Cases (Value $ Var stateVar) cases defaults]
-                  vs = [Voided (Value $ Var event_var)]
+                  es = [Unused (Value $ Var event_var), Cases (Value $ Var stateVar) cases defaults]
                   event_var = head eventNames
                   call_unhandled = ExprS $ FunCall (unhandled_f e) [Value $ Var event_var]
                   call_ev_in s e' = ExprS $ FunCall (qualify (s, mangleEv e')) (if e == e' then [Value $ Var event_var] else [])
@@ -435,7 +434,7 @@ lowerMachine cfg ssyms (StateMachine smName, g') = [
                   panic_s = disqualifyTag smName ++ "{%s}: Invalid event ID\n"
 
         stateEventFun :: State TaggedName -> Happening -> State TaggedName -> Bool -> Def QualifiedName
-        stateEventFun st h st' do_exit = FunDef f_name eventNames (Internal, ty) [] $ logAState ++ map Voided vs ++ map ExprS es
+        stateEventFun st h st' do_exit = FunDef f_name eventNames (Internal, ty) [] $ logAState ++ map Unused vs ++ map ExprS es
             where f_name = qualify (sName smName st, mangleEv $ event h)
                   ty = case event h of
                               (Event t) -> Ty t :-> Void
@@ -459,11 +458,9 @@ lowerMachine cfg ssyms (StateMachine smName, g') = [
                   psOf (p    :-> _) = [if isEventTy p (event h) then Value $ Var event_var else Null]
                   apply_se (f, FuncTyped _) = undefined -- See ticket #15, harder than it seems at first.
                   apply_se (f, _) = FunCall (qualify f) (psOf (snd $ syms ! f))
-                  vs = case (event h) of
-                          EventEnter -> []
-                          EventExit -> []
-                          EventAny -> []
-                          _ -> [Value $ Var event_var]
+                  vs = case event h of
+                              (Event t) -> [Value $ Var event_var]
+                              otherwise -> []
                   es = case h of
                          (Happening _ ses [])                        -> [apply_se se | se <- ses] ++ (if do_exit then [call_exit] else []) ++ [assign_state, call_enter]
                          (Happening _ ses fs) | elem NoTransition fs -> [apply_se se | se <- ses]
